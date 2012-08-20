703b1a4af9556bce8acaa19b188466f05d18f674
Darlan Cavalcante Moreira <darcamo@gmail.com>
2012-08-20 18:14:54 +0000

    Pathloss module
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@57 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

---
+++ comm/pathloss.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Implement classes for several Path loss models.
+
+The base class PathLossBase implements the code common to every path loss
+model and only two methods need to be implemented in subclasses:
+which_distance_dB and _calc_deterministic_path_loss_dB.
+
+The most common usage of a path loss class is to instantiate an object of
+the desired path loss model and then call the calc_path_loss_dB or the
+calc_path_loss methods to actually calculate the path loss.
+"""
+
+import conversion
+import numpy as np
+
+
+class PathLossBase(object):
+    """Base class for the different Path Loss models.
+
+    The common interface for the path loss classes is provided by the
+    calc_path_loss_dB or the calc_path_loss methods to actually calculate
+    the path loss for a given distance, as well as the which_distance_dB or
+    which_distance methods to determine the distance that yields the given
+    path loss.
+
+    Each subclass of PathLossBase NEED TO IMPLEMENT only the
+    "which_distance_dB" and the "_calc_deterministic_path_loss_dB"
+    functions.
+
+    If the use_shadow_bool is set to True then calling calc_path_loss_dB or
+    calc_path_loss will take the shadowing specified in the sigma_shadow
+    variable into account. However, shadowing is not taken into account in
+    the which_distance_dB and which_distance functions regardless of the
+    value of the use_shadow_bool variable.
+    """
+
+    def __init__(self, ):
+        """
+        """
+        self.sigma_shadow = 8  # Shadow standard deviation
+        self.use_shadow_bool = False
+
+    #
+    # xxxxx Start - Implemented these functions in subclasses xxxxxxxxxxxxx
+    def which_distance_dB(self, PL):
+        """
+        Calculates the distance that yields the given path loss (in dB).
+
+        Arguments:
+        - `PL`: Path Loss (in dB)
+        """
+        # Raises an exception if which_distance_dB is not implemented in a
+        # subclass
+        raise NotImplementedError('which_distance_dB must be reimplemented in the {0} class'.format(self.__class__.__name__))
+
+    def _calc_deterministic_path_loss_dB(self, d):
+        """Calculates the Path Loss (in dB) for a given distance (in Km)
+        without including the shadowing.
+
+        Arguments:
+        - `d`: Distance (in Km)
+        """
+        raise NotImplementedError('_calc_deterministic_path_loss_dB must be reimplemented in the {0} class'.format(self.__class__.__name__))
+    # xxxxx End - Implemented these functions in subclasses xxxxxxxxxxxxxxx
+    #
+
+    def calc_path_loss_dB(self, d):
+        """Calculates the Path Loss (in dB) for a given distance (in
+        kilometers).
+
+        Note that the returned value is positive, but should be understood
+        as "a loss".
+
+        Arguments:
+        - `d`: Distance (in Km)
+        """
+        if self.use_shadow_bool == True:
+            PL = self._calc_deterministic_path_loss_dB(d) + self.gen_shadow()
+        else:
+            PL = self._calc_deterministic_path_loss_dB(d)
+        return PL
+
+    def calc_path_loss(self, d):
+        """Calculates the path loss (in linear scale) for a given distance
+        (in kilometers).
+
+        Arguments:
+        - `d`: Distance (in Km)
+        """
+        pl = conversion.dB2Linear(-self.calc_path_loss_dB(d))
+        return pl
+
+    def which_distance(self, pl):
+        """Calculates the required distance (in kilometers) to achieve the
+        given path loss. It is the inverse of the calcPathLoss function.
+
+        Arguments:
+        - `pl`: Path loss (in linear scale)
+        """
+        d = self.which_distance_dB(-conversion.linear2dB(pl))
+        return d
+
+    def gen_shadow(self, dim=1):
+        """Generates samples for the shadowing with the lognormal standard
+        deviation given by the sigmaShadow property. The "dim" variable
+        specifies the dimension of the output and defaults to a scalar.
+
+        Arguments:
+        - `dim`: Dimensions (scalar) of the output
+        """
+        shadow = conversion.dB2Linear(np.random.randn(dim) * self.sigma_shadow)
+        if dim == 1:
+            # Return shadow as a scalar instead of as an array with a
+            # single element
+            return shadow[0]
+        else:
+            return shadow
+
+
+class PathLossFreeSpace(PathLossBase):
+    """Class to calculate the Path Loss in the free space.
+
+    The common interface for the path loss classes is provided by the
+    calc_path_loss_dB or the calc_path_loss methods to actually calculate
+    the path loss for a given distance, as well as the which_distance_dB or
+    which_distance methods to determine the distance that yields the given
+    path loss.
+
+    For the path loss in free space you also need to set the `n` variable,
+    corresponding to the path loss coefficient, and the `fc` variable,
+    corresponding to the frequency. The `n` variable defaults to 2 and `fc`
+    defaults to 900e6 (that is, 900MHz).
+
+    Ex: Determining the path loss in the free space for a distance of 1Km
+    (without considering shadowing).
+
+    >>> pl = PathLossFreeSpace()
+    >>> pl.calc_path_loss(1)        # linear scale
+    7.036193308495632e-10
+    >>> pl.calc_path_loss_dB(1)     # log scale
+    91.526622374835199
+
+    Ex: Determining the distance (in Km) that yields a path loss of 90dB.
+    >>> pl.which_distance_dB(90)
+    0.83882020174144778
+    """
+
+    def __init__(self, ):
+        #super(PathLossFreeSpace, self).__init__()
+        PathLossBase.__init__(self)
+        self.n = 2       # Path Loss Coefficient
+        self.fc = 900e6  # Frequency of the central carrier
+
+    def which_distance_dB(self, PL):
+        """Calculates the required distance (in kilometers) to achieve the
+        given path loss (in dB). It is the inverse of the calcPathLoss
+        function.
+
+        $10^(PL/(10*n) - log10(fc) + 4.377911390697565)$
+        d = obj.whichDistance(dB2Linear(-PL));
+
+        Arguments:
+        - `PL`: Path Loss (in dB)
+        Returns:
+        - Distance (in Km)
+        """
+        d = 10. ** (PL / (10. * self.n) - np.log10(self.fc) + 4.377911390697565)
+        return d
+
+    def _calc_deterministic_path_loss_dB(self, d):
+        """Calculates the Path Loss (in dB) for a given distance (in
+        kilometers).
+
+        Note that the returned value is positive, but should be understood
+        as "a loss".
+
+        For d in Km and self.fc in Hz, the free space Path Loss is given by
+        PL = 10 n ( log10(d) +log10(f) - 4.3779113907 )
+
+        Arguments:
+        - `d`: Distance (in Km)
+        """
+        PL = 10 * self.n * (np.log10(d) + np.log10(self.fc) - 4.377911390697565)
+        return PL
+
+
+class PathLoss3GPP1(PathLossBase):
+    """Class to calculate the Path Loss according to the model from 3GPP
+    (scenario 1). That is, the Path Loss (in dB) is equal to
+    $128.1 + 37.6*\log10(d)$.
+
+    This model is valid for LTE assumptions and at 2GHz frequency, where
+    the distance is in Km.
+
+    Ex: Determining the path loss in the free space for a distance of 1Km
+    (without considering shadowing).
+
+    >>> pl = PathLoss3GPP1()
+    >>> pl.calc_path_loss(1)        # linear scale
+    1.5488166189124858e-13
+    >>> pl.calc_path_loss_dB(1)     # log scale
+    128.09999999999999
+
+    Ex: Determining the distance (in Km) that yields a path loss of 90dB.
+    >>> pl.which_distance_dB(90)
+    0.09698445455772262
+    """
+
+    def __init__(self):
+        # super(PathLossFreeSpace, self).__init__()
+        PathLossBase.__init__(self)
+
+    def which_distance_dB(self, PL):
+        """Calculates the required distance (in kilometers) to achieve the
+        given path loss (in dB). It is the inverse of the calcPathLoss
+        function.
+
+        Arguments:
+        - `PL`: Path loss (in dB)
+        Returns:
+        - Distance (in Km)
+        """
+        d = 10 ** ((PL - 128.1) / 37.6)
+        return d
+
+    def _calc_deterministic_path_loss_dB(self, d):
+        """Calculates the Path Loss (in dB) for a given distance (in
+        kilometers).
+
+        Note that the returned value is positive, but should be understood
+        as "a loss".
+
+        For d in Km and self.fc in Hz, the free space Path Loss is given by
+        PL = 10n ( log10(d) +log10(f) - 4.3779113907 )
+
+        Arguments:
+        - `d`: Distance (in Km)
+        """
+        PL = 128.1 + 37.6 * np.log10(d)
+        return PL
+
+
+# xxxxx Main xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    print "{0} executed".format(__file__)
