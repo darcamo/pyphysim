9cd1095c11dd244c1e2f22853e5bda43192aec65
Darlan Cavalcante Moreira <darcamo@gmail.com>
2012-08-28 03:11:11 +0000

    tests for the OFDM module
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@60 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

---
+++ tests/comm_package_test.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the modules in the coom package.
+
+
+Each module has several doctests that we run in addition to the unittests
+defined here.
+"""
+
+import unittest
+import doctest
+import sys
+
+import numpy as np
+from comm import modulators, blockdiagonalization, ofdm, mimo, pathloss, waterfilling
+
+
+# UPDATE THIS CLASS if another module is added to the comm package
+class CommDoctestsTestCase(unittest.TestCase):
+    """Teste case that run all the doctests in the modules of the comm
+    package."""
+
+    def test_modulators(self):
+        """Run doctests in the modulators module."""
+        doctest.testmod(modulators)
+
+    def test_blockdiagonalization(self, ):
+        """Run doctests in the blockdiagonalization module."""
+        doctest.testmod(blockdiagonalization)
+
+    def test_ofdm(self, ):
+        """Run doctests in the ofdm module."""
+        doctest.testmod(ofdm)
+
+    def test_mimo(self, ):
+        """Run doctests in the mimo module."""
+        doctest.testmod(mimo)
+
+    def test_pathloss(self, ):
+        """Run doctests in the pathloss module."""
+        doctest.testmod(pathloss)
+
+    def test_waterfilling(self, ):
+        """Run doctests in the waterfilling module."""
+        doctest.testmod(waterfilling)
+
+
+# TODO: Create test cases for the other methods and classes in each module,
+# even for methods that are already doctested.
+
+class OfdmTestCase(unittest.TestCase):
+    def setUp(self):
+        """Called before each test."""
+        from comm.ofdm import OFDM
+        self.ofdm_object = OFDM(64, 16, 52)
+
+    # TODO: change "lalala" to test
+    def lalala_ofdm_set_parameters(self):
+        # Test regular usage
+        self.assertEqual(self.ofdm_object.fft_size, 64)
+        self.assertEqual(self.ofdm_object.cp_size, 16)
+        self.assertEqual(self.ofdm_object.num_used_subcarriers, 52)
+        self.ofdm_object.set_parameters(128, 32, 100)
+        self.assertEqual(self.ofdm_object.fft_size, 128)
+        self.assertEqual(self.ofdm_object.cp_size, 32)
+        self.assertEqual(self.ofdm_object.num_used_subcarriers, 100)
+
+        # Test if an exception is raised when any invalid parameters are
+        # passed to set_parameters
+
+        # Raises an exception if number of used subcarriers is negative
+        self.assertRaises(ValueError, self.ofdm_object.set_parameters, 64, 16, 51)
+        # Raises an exception if number of used subcarriers is greater than
+        # the fft_size
+        self.assertRaises(ValueError, self.ofdm_object.set_parameters, 64, 16, 70)
+        # Raises an exception if cp_size is negative
+        self.assertRaises(ValueError, self.ofdm_object.set_parameters, 64, -2, 52)
+        # Raises an exception if cp_size is greater than the fft_size
+        self.assertRaises(ValueError, self.ofdm_object.set_parameters, 64, 65, 52)
+
+    def test_prepare_input_signal(self, ):
+        input_signal = np.r_[1:53]  # 52 elements -> exactly the number of
+                                    # used subcarriers in the OFDM object
+
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        # Lets first test the case where we use exactly 52 subcarriers from
+        # the 64 available subcarriers. That is, no zeropadding is needed.
+        (zeropad, num_ofdm_symbols) = self.ofdm_object._calc_zeropad(input_signal.size)
+        self.assertEqual(zeropad, 0)
+        self.assertEqual(num_ofdm_symbols, 1)
+
+        expected_data = np.array(
+            [[0., 27., 28., 29., 30., 31., 32., 33., 34., 35., 36., 37., 38.,
+              39., 40., 41., 42., 43., 44., 45., 46., 47., 48., 49., 50., 51.,
+              52., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 2., 3., 4.,
+              5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18.,
+              19., 20., 21., 22., 23., 24., 25., 26.]])
+        np.testing.assert_array_equal(self.ofdm_object._prepare_input_signal(input_signal), expected_data)
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        # Now lets change the number of used subcarriers and repeat the
+        # tests so that the case when zeropad is performed is also tested.
+        self.ofdm_object.num_used_subcarriers = 60
+        (zeropad, num_ofdm_symbols) = self.ofdm_object._calc_zeropad(input_signal.size)
+        # We used 60 subcarriers, but we have 52 elements -> We need to add
+        # 8 zeros at the end of the unput data
+        self.assertEqual(zeropad, 8)
+        # But we still have only one OFDM symbol
+        self.assertEqual(num_ofdm_symbols, 1)
+
+        expected_data2 = np.array(
+            [[0., 31., 32., 33., 34., 35., 36., 37., 38., 39., 40., 41., 42.,
+              43., 44., 45., 46., 47., 48., 49., 50., 51., 52., 0., 0., 0.,
+              0., 0., 0., 0., 0., 0., 0., 0., 1., 2., 3., 4., 5., 6., 7., 8.,
+              9., 10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20., 21.,
+              22., 23., 24., 25., 26., 27., 28., 29., 30., ]])
+        np.testing.assert_array_equal(self.ofdm_object._prepare_input_signal(input_signal), expected_data2)
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        # Now lets test the case when we use all subcarriers (but still
+        # with zeropadding)
+        self.ofdm_object.num_used_subcarriers = 64
+        (zeropad, num_ofdm_symbols) = self.ofdm_object._calc_zeropad(input_signal.size)
+        self.assertEqual(zeropad, 12)
+        # But we still have only one OFDM symbol
+        self.assertEqual(num_ofdm_symbols, 1)
+        # Notice that in this case the DC subcarrier is used
+        expected_data3 = np.array(
+            [[33., 34., 35., 36., 37., 38., 39., 40., 41., 42., 43., 44., 45.,
+              46., 47., 48., 49., 50., 51., 52., 0., 0., 0., 0., 0., 0., 0.,
+              0., 0., 0., 0., 0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11.,
+              12., 13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23., 24.,
+              25., 26., 27., 28., 29., 30., 31., 32.]])
+        np.testing.assert_array_equal(self.ofdm_object._prepare_input_signal(input_signal), expected_data3)
+
+
+    def test_modulate(self):
+        input_signal = np.r_[1:105]  # Exactly two OFDM symbols (with 52 used
+                                     # subcarriers)
+
+        # xxxxx First lets try without cyclic prefix xxxxxxxxxxxxxxxxxxxxxx
+        self.ofdm_object.set_parameters(64, 0, 52)
+        (zeropad, num_ofdm_symbols) = self.ofdm_object._calc_zeropad(input_signal.size)
+        self.assertEqual(zeropad, 0)
+        # But we still have only one OFDM symbol
+        self.assertEqual(num_ofdm_symbols, 2)
+
+        input_ifft = self.ofdm_object._prepare_input_signal(input_signal)
+        expected_data = np.hstack([
+                np.fft.ifft(input_ifft[0, :]),
+                np.fft.ifft(input_ifft[1, :]),
+                ])
+
+        np.testing.assert_array_almost_equal(
+            self.ofdm_object.modulate(input_signal), expected_data)
+
+        # Lets test each OFDM symbol individually
+        np.testing.assert_array_almost_equal(
+            self.ofdm_object.modulate(input_signal[0:52]), expected_data[0:64])
+        np.testing.assert_array_almost_equal(
+            self.ofdm_object.modulate(input_signal[52:]), expected_data[64:])
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        # xxxxx Now lets test with a cyclic prefix xxxxxxxxxxxxxxxxxxxxxxxx
+        self.ofdm_object.set_parameters(64, 4, 52)
+        expected_data2 = expected_data[0:64]
+        expected_data2 = np.hstack([expected_data2[-self.ofdm_object.cp_size:], expected_data[0:64]])
+        np.testing.assert_array_almost_equal(
+            self.ofdm_object.modulate(input_signal[0:52]), expected_data2)
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# TODO: Test the other methods
+
+if __name__ == "__main__":
+    unittest.main()
