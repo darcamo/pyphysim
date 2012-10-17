4655ca26f0d4dc4ebf45e13825ed2f20b7a2ef2f
Darlan Cavalcante Moreira <darcamo@gmail.com>
2012-10-17 22:21:15 +0000

    - cell/shapes.py
      xxxxxxxxxx Shape Class xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
      Added the is_point_inside_shape method.
    
      Implemented the get_border_point method (still needs validation and maybe
      fixing bugs).
    
      Implemented the plot method.
      xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
    
      Added the Rectangle class in cell.shapes.py
    
      Added the Circle class in  cell.shapes.py
    
    
      
    - cell/cell.py
    
      Added the Node class.
    
      Implementation of more methods of the CellBaseClass.
    
    
    
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@102 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

--- cell/cell.py
+++ cell/cell.py
@@ -3,8 +3,51 @@
 
 """Module that implements Cell related classes."""
 
+from matplotlib import pylab
 
 from shapes import Coordinate
+
+
+class Node(Coordinate):
+    """Class representing a node in the network.
+    """
+    def __init__(self, pos):
+        """
+
+        Arguments:
+        - `pos`: The position of the node (a complex number)
+        """
+        Coordinate.__init__(self, pos)
+        self.plot_marker = '*'
+        self.marker_color = 'r'
+
+    def plot_node(self, ax=None):
+        """Plot the node using the matplotlib library.
+
+        If an axes 'ax' is specified, then the node is added to that
+        axes. Otherwise a new figure and axes are created and the node is
+        plotted to that.
+
+        Arguments:
+        - `ax`: A matplotlib axes
+
+        """
+        stand_alone_plot = False
+
+        if (ax is None):
+            # This is a stand alone plot. Lets create a new axes.
+            ax = pylab.axes()
+            stand_alone_plot = True
+
+        ax.plot(self.pos.real,
+                self.pos.imag,
+                marker=self.plot_marker,
+                markerfacecolor=self.marker_color,
+                markeredgecolor=self.marker_color)
+
+        if stand_alone_plot is True:
+            ax.plot()
+            pylab.show()
 
 
 class CellBaseClass(Coordinate):
@@ -17,8 +60,35 @@ class CellBaseClass(Coordinate):
         Coordinate.__init__(self, pos)
         self.id = cell_id
         self.radius = radius  # Should be in a Shape class
+        self._users = []
+
+    def _get_num_users(self):
+        return len(self._users)
+    num_users = property(_get_num_users)
+
+    def _get_users(self):
+        return self._users
+    users = property(_get_users)
+
+    def delete_users(self, ):
+        """Delete all users from the cell.
+        """
+        self._users = []
+
+    def add_user(self, new_user, relative_pos_bool):
+        """Adds a new user to the cell.
+
+        Arguments:
+        - `new_user`: New user to be added to the cell.
+        - `relative_pos_bool`: Indicates if the 'pos' attribute of the user
+                               is relative to the center of the cell or not
+
+        """
+        TODO: Implement-me
 
 
 if __name__ == '__main__':
     c = CellBaseClass(0, 1)
     print c
+    #n = Node(1+4j)
+    #n.plot_node()
--- cell/shapes.py
+++ cell/shapes.py
@@ -7,6 +7,8 @@ Each shape knows how to plot itself.
 """
 
 import numpy as np
+from matplotlib import pylab
+from matplotlib.patches import Polygon
 
 
 class Coordinate(object):
@@ -24,7 +26,7 @@ class Coordinate(object):
         """
         self.pos = pos
 
-    def calc_didt(self, other):
+    def calc_dist(self, other):
         """Calculates the distance to another coordinate.
 
         Arguments:
@@ -70,15 +72,114 @@ class Shape(Coordinate):
         raise NotImplementedError('get_vertex_positions still needs to be implemented in the {0} class'.format(self.__class__.__name__))
 
     # xxxxx vertex property xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    def _get_vertexes(self):
+    def _get_vertices(self):
         vertex_positions = self._get_vertex_positions()
-        num_vertexes = vertex_positions.shape,
-
-        vertex_positions = self.pos + Shape._rotate(vertex_positions, self.rotation)
+        vertex_positions = self.pos + Shape._rotate(
+            vertex_positions, self.rotation)
         return vertex_positions
 
-    vertexes = property(_get_vertexes)
+    vertices = property(_get_vertices)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    def is_point_inside_shape(self, point):
+        """Tests is a point is inside the shape.
+
+        Arguments:
+        - `point`: A single complex number.
+
+        """
+        import matplotlib.nxutils as mnx
+        # pnpoly returns 1 if point is inside the polygon and 0 otherwise
+        return mnx.pnpoly(point.real, point.imag, conv_N_complex_array_to_N_by_2_real_matrix(self.vertices)) == 1
+
+    # TODO: Test and Validate this method
+    def get_border_point(self, angle, ratio):
+        """Calculates the coordinate of the point that intercepts the
+        border of the cell if we go from the origin with a given angle
+        (in degrees).
+
+        Arguments:
+        - `angle`: Angle (in degrees)
+        - `ratio`: Ratio (between 0 and 1)
+
+        """
+        # Get the vertices (WITH rotation, but WITHOUT translation)
+        vertices_no_trans = Shape._rotate(self._get_vertex_positions(),
+                                         self.rotation)
+        angle_rad = np.pi * angle / 180.
+
+        # Which point we get if we walk a distance of cell radius in the
+        # desired angle direction?
+        point = self.radius * np.exp(angle_rad)
+
+        # Calculates the distance of this point to all vertices and finds
+        # the closest vertices
+        dists = np.abs(self.vertices - point)
+        # Get the two closest vertices from point
+        closest_vertices = vertices_no_trans[np.argsort(dists)[:2]]
+
+        # The equation of a straight line is given by "y = ax + b". We have
+        # two points in this line (the two closest vertices) and we can use
+        # them to find 'a' and 'b'.
+        a = closest_vertices[0] - closest_vertices[1]
+        a = a.imag / a.real
+        b = closest_vertices[1].imag - a * closest_vertices[1].real
+
+        # Now lets find the equation of the straight line from the origin
+        # of the shape with the desired angle.
+        a2 = np.tan(angle_rad)
+        b2 = 0
+        # The equation of this line is then given by "y2 = a2 x2 + b2"
+
+        # The point in the border of the shape is the point where these two
+        # equations meet. That is, "a*x+b == a2*x+b2"
+        x = (b2-b) / (a-a2)
+        y = a*x + b
+
+        point = complex(x, y) * ratio + self.pos
+
+        return point
+
+    def plot(self, ax=None):
+        """Plot the shape using the matplotlib library.
+
+        If an axes 'ax' is specified, then the shape is added to that
+        axes. Otherwise a new figure and axes are created and the shape is
+        plotted to that.
+
+        Arguments:
+        - `ax`: A matplotlib axes
+        """
+        stand_alone_plot = False
+
+        if (ax is None):
+            # This is a stand alone plot. Lets create a new axes.
+            ax = pylab.axes()
+            stand_alone_plot = True
+
+        if self.fill_face_bool:
+            # Matplotlib does not seem to allow us to set a different alpha
+            # value for the face and the edges. Therefore, we will need to
+            # plot twice to get that effect.
+            polygon_face = Polygon(
+                conv_N_complex_array_to_N_by_2_real_matrix(self.vertices),
+                True,
+                facecolor=self.fill_color,
+                edgecolor='none',  # No edges
+                alpha=self.fill_opacity)
+            ax.add_patch(polygon_face)
+
+        polygon_edges = Polygon(
+            conv_N_complex_array_to_N_by_2_real_matrix(self.vertices),
+            True,
+            facecolor='none',  # No face
+            alpha=1)
+
+        ax.add_patch(polygon_edges)
+
+        if stand_alone_plot is True:
+            ax.plot()
+            pylab.show()
 
     @staticmethod
     def _rotate(cur_pos, angle):
@@ -130,13 +231,122 @@ class Hexagon(Shape):
         angles = np.linspace(0, 240, 5) * np.pi / 180.
 
         for k in range(5):
-            vertexPositions[k + 1] = vertexPositions[k] + \
-            self.radius * np.exp(angles[k] * 1j)
+            vertexPositions[k + 1] = vertexPositions[k] + self.radius * np.exp(angles[k] * 1j)
         return vertexPositions
 
 
+class Rectangle(Shape):
+    """Rectangle shape class.
+    """
+
+    def __init__(self, A, B, rotation=0):
+        """Initializes the shape.
+
+        The rectangle is initialized from two coordinates as well as from
+        the rotation.
+
+        Arguments:
+        - `A`: First coordinate.
+        - `B`: Second coordinate.
+        - `rotation`: Rotation of the rectangle in degrees (a positive real
+                      number).
+
+        """
+        central_pos = (A + B) / 2
+        radius = np.abs(B - central_pos)
+        Shape.__init__(self, central_pos, radius, rotation)
+        self._lower_coord = A
+        self._upper_coord = B
+
+    def _get_vertex_positions(self):
+        """Calculates the vertex positions ignoring any rotation and considering
+        that the shape is at the origin (rotation and translation will be
+        added automatically later).
+
+        Must return a one-dimensional numpy array (complex dtype) with the
+        vertex positions.
+
+        """
+        vertex_positions = np.zeros(4, dtype=complex)
+        A = self._lower_coord - self.pos
+        B = self._upper_coord - self.pos
+        vertex_positions[0] = A
+        vertex_positions[1] = complex(B.real, A.imag)
+        vertex_positions[2] = B
+        vertex_positions[3] = complex(A.real, B.imag)
+        return vertex_positions
+
+
+class Circle(Shape):
+    """Circle shape class.
+    """
+
+    def __init__(self, pos, radius):
+        """Initializes the shape.
+
+        A circle is initialized only from a coordinate and a radius.
+
+        Arguments:
+        - `pos`: Coordinate of the center of the circle.
+        - `radius`: Circle's radius.
+
+        """
+        Shape.__init__(self, pos, radius)
+
+    def _get_vertex_positions(self):
+        # 180 points from 0 to 2pi
+        angles = np.linspace(0, 2 * np.pi, 180)
+        vertex_positions = self.radius * np.exp(1j * angles)
+        return vertex_positions
+
+
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-if __name__ == '__main__':
+# TODO: Create a better name and rename this method (use ropemacs for that)
+def conv_N_complex_array_to_N_by_2_real_matrix(a):
+    """Convert an array of complex number to a matrix of real numbers. The
+    first coloumn of the matrix is the real part while the second column is
+    the imaginary part of the original array.
+
+    """
+    num_elem = np.size(a)
+    a.dtype = a.real.dtype
+    a = np.reshape(a, (num_elem, 2))
+    return a
+
+
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+if __name__ == '__main__1':
+    ax = pylab.axes()
     h = Hexagon(0, 1)
-    print h.height
-    print h._get_vertex_positions()
+    point = h.get_border_point(30, 1)
+    print point
+    h.plot(ax)
+    ax.plot(point.real, point.imag, 'r*')
+    pylab.show()
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    ax = pylab.axes()
+
+    h = Hexagon(0, 1)
+    h.rotation = 30
+    h.fill_face_bool = True
+
+    pylab.hold(True)
+    r = Rectangle(0, 2 + 1j, 0)
+    r.rotation = 15
+    r.fill_face_bool = True
+    r.fill_color = 'b'
+
+    c = Circle(0, 1)
+    c.fill_face_bool = True
+    c.fill_color = 'g'
+
+    r.plot(ax)
+    h.plot(ax)
+    c.plot(ax)
+
+
+    ax.plot()
+    plt.axis('equal')
+    plt.show()
