df5b54dbebfd4d9a43aa6cb14f57fbd64bf781d5
Darlan Cavalcante Moreira <darcamo@gmail.com>
2012-10-24 15:00:33 +0000

    - tests/cell_package_test.py   Tests for the Hexagon class (and some for the Shape class)     Implemented a test for the _get_vertices property.     Implemented a test for the is_point_inside_shape method.     Implemented a test for the is_point_inside_shape method.     Implemented a test for the get_border_point method.
      Tests for the Rectangle class.
        Implemented a test for the _get_vertexes_positions method.
    
      Tests for the Circle Class
        Implemented a test for the get_border_point method.
        Implemented a test for the _get_vertexes_positions method (incomplete).
        
    
    
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@117 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

--- tests/cell_package_test.py
+++ tests/cell_package_test.py
@@ -81,18 +81,14 @@ class ShapeTestCase(unittest.TestCase):
         # This method uses the _get_vertex_positions method. Therefore, it
         # can only be tested in subclasses of the Shape class.
         #
-        # TODO: After implementing a test for this method in a subclass
-        # write here in place of this comment the name f the subclass where
-        # this method is tested.
+        # This method was tested in the Hexagon unittests.
         pass
 
     def test_get_border_point(self):
         # This method uses the _get_vertex_positions method. Therefore, it
         # can only be tested in subclasses of the Shape class.
         #
-        # TODO: After implementing a test for this method in a subclass
-        # write here in place of this comment the name f the subclass where
-        # this method is tested.
+        # This method was tested in the Hexagon unittests.
         pass
 
     def test_rotate(self):
@@ -121,8 +117,6 @@ class HexagonTestCase(unittest.TestCase):
                                np.sqrt(3.0) * self.H3.radius / 2.0)
 
     def test_get_vertex_positions(self):
-        # TODO: Implement-me
-        pass
         # Even though H1 and H2 have different position and rottion, they
         # have the same radius and since _get_vertex_positions does not
         # account translation and rotation then the _get_vertex_positions
@@ -148,27 +142,161 @@ class HexagonTestCase(unittest.TestCase):
         np.testing.assert_array_almost_equal(expected_vertexes2,
                                              obtained_vertexes2)
 
+    def test_get_vertices(self):
+        # H1 does not have any translation or rotation. Therefore,
+        # _get_vertex_positions should return the same values as the
+        # vertices property
+        np.testing.assert_array_almost_equal(self.H1._get_vertex_positions(),
+                                             self.H1.vertices)
+
+        # H2 has translation.
+        np.testing.assert_array_almost_equal(
+            self.H2._get_vertex_positions() + complex(2, 3),
+            self.H2.vertices)
+
+        # H3 has rotation and translation.
+        np.testing.assert_array_almost_equal(
+            shapes.Shape._rotate(self.H3._get_vertex_positions(), 30) + 3 + 5j,
+            self.H3.vertices)
+
     def test_is_point_inside_shape(self, ):
-        # TODO: Implement-me
-        pass
+        # If the point is exactly in the shape's border, such as the
+        # shape's vertexes, then is_point_inside_shape could return either
+        # true or false.
+        for v in self.H1.vertices:
+            # Because the hexagons center is at the origin, we know that if
+            # we multiply a vertex by a number lower then one it is
+            # guaranteed be inside the shape and vice-verse.
+            self.assertTrue(self.H1.is_point_inside_shape(0.9999999 * v))
+            self.assertFalse(self.H1.is_point_inside_shape(1.0000001 * v))
+
+        # This test is very imcomplete. If any bugs are found in
+        # is_point_inside_shape add tests for them here
 
     def test_get_border_point(self):
-        # TODO: Implement-me
-        pass
+        # Test for an angle of 30 degrees (that is one of the vertexes)
+        point_30_degrees = self.H3.get_border_point(30., 1.)
+        point_30_degrees_expected = self.H3.pos + self.H3.radius * np.exp(1j * np.pi / 6.)
+        self.assertAlmostEqual(point_30_degrees, point_30_degrees_expected)
+
+        # Test for an angle of 30 degrees, but now with a ratio of 0.8
+        point_30_degrees_ratio08 = self.H3.get_border_point(30., 0.8)
+        point_30_degrees_expected_ratio08 = self.H3.pos + 0.8 * self.H3.radius * np.exp(1j * np.pi / 6.)
+        self.assertAlmostEqual(point_30_degrees_ratio08, point_30_degrees_expected_ratio08)
+
+        # Test for an angle of 180 degrees
+        point_180_degrees = self.H3.get_border_point(180., 1.)
+        point_180_degrees_expected = self.H3.pos - self.H3.height
+        self.assertAlmostEqual(point_180_degrees, point_180_degrees_expected)
+
+        # Test for an angle of 180 degrees but with a ratio of 0.5
+        point_180_degrees_ratio05 = self.H3.get_border_point(180, 0.5)
+        point_180_degrees_expected_ratio05 = self.H3.pos - (self.H3.height / 2.0)
+        self.assertAlmostEqual(point_180_degrees_ratio05, point_180_degrees_expected_ratio05)
 
 
-# TODO: Implement-me
 class RectangleTestCase(unittest.TestCase):
-    def setUp(self):
-        """Called before each test."""
-        pass
+    def test_get_vertex_positions(self):
+        A1 = 0 + 0j
+        B1 = 1 + 1j
+        R1 = shapes.Rectangle(A1, B1)
+        # With these coordinates, the central position of R1 is 0.5 + 0.5j
+        self.assertAlmostEqual(R1.pos, 0.5 + 0.5j)
+
+        # The rectangle vertexes are then -0.5-0.5j, 0.5-0.5j, 0.5+0.5j and
+        # -0.5+0.5j
+        np.testing.assert_array_almost_equal(
+            R1._get_vertex_positions(),
+            np.array([-0.5 - 0.5j, 0.5 - 0.5j, 0.5 + 0.5j, -0.5 + 0.5j]))
+
+        # Note that though A2 and B2 are different from A1 and B1, they are
+        # all coordinates of the same rectangle and therefore R2 should
+        # have the same position and vertexes as R1.
+        A2 = 1 + 0j
+        B2 = 0 + 1j
+        R2 = shapes.Rectangle(A2, B2)
+        self.assertAlmostEqual(R1.pos, R2.pos)
+        np.testing.assert_almost_equal(R1._get_vertex_positions(), R2._get_vertex_positions())
+
+        # Now lets create a rectangle with rotation and translates to a
+        # different position. The _get_vertex_positions method should not
+        # be affected by this
+        trans_step = 1.4 - 3j
+        R3 = shapes.Rectangle(A1 + trans_step, B1 + trans_step, 30)
+        np.testing.assert_almost_equal(R1._get_vertex_positions(), R3._get_vertex_positions())
 
 
 # TODO: Implement-me
 class CircleTestCase(unittest.TestCase):
     def setUp(self):
         """Called before each test."""
-        pass
+        self.C1 = shapes.Circle(3 - 5j, 2)
+
+    def test_get_border_point(self):
+        C1 = self.C1
+        point = C1.get_border_point(90, 1)
+        expected_point = C1.pos + 1j * C1.radius
+        self.assertAlmostEqual(point, expected_point)
+
+        # from matplotlib import pylab
+        # ax = pylab.axes()
+        # C1.plot(ax)
+        # for angle in np.arange(0, 359, 30):
+        #     border_point = C1.get_border_point(angle, 1)
+        #     border_point2  = C1.get_border_point(angle, 0.5)
+        #     ax.plot(border_point.real, border_point.imag, 'go')
+        #     ax.plot(border_point2.real, border_point2.imag, 'bo')
+        # pylab.show()
+
+    def test_get_vertex_positions(self):
+        # TODO: Finish the implementation
+        #
+        # The _get_vertex_positions in the Circle class should return
+        # border points for angles in 0:2:360
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        def positivy_angle_rad(angle):
+            """Return the positive angle corresponding to angle.
+
+            Arguments:
+            - `angle`: An angle (in radians)
+
+            """
+            if angle > 0:
+                return angle
+            else:
+                return 2 * np.pi + angle
+
+        def get_sorted_array_by_angle(x):
+            """Function to return the complex array sorted by the angles
+            ("positivated angles")
+
+            """
+            angles_rad = np.angle(x)
+            angles_rad = map(positivy_angle_rad, angles_rad)
+            return x[np.argsort(angles_rad)]
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        #
+
+        angles_rad = np.linspace(0, 2 * np.pi, 180)
+        expected_vertexes = np.array(
+            map(self.C1.get_border_point,
+                angles_rad, np.ones(angles_rad.shape))) - self.C1.pos
+        expected_vertexes = get_sorted_array_by_angle(expected_vertexes)
+        vertexes = get_sorted_array_by_angle(self.C1._get_vertex_positions())
+
+        np.testing.assert_array_almost_equal(expected_vertexes, vertexes)
+        #print self.C1._get_vertex_positions()
+        #print expected_vertexes
+
+        # print f(expected_vertexes)
+        # print
+        # print f(self.C1._get_vertex_positions())
+
+
+
+        expected_vertexes[np.argsort(np.angle(expected_vertexes))]
+
+
 
 
 class ShapesModuleMethodsTestCase(unittest.TestCase):
