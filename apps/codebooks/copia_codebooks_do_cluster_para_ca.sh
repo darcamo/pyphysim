15ee48054f82988554b3f3678f3324059991df34
Darlan Cavalcante Moreira <darcamo@gmail.com>
2012-10-29 17:24:17 +0000

    Added codebook related application.
    This is half baked and maybe should be removed in the future, but I'm
    adding it for now.
    
    
    
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@128 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

---
+++ apps/codebooks/copia_codebooks_do_cluster_para_ca.sh
@@ -0,0 +1,4 @@
+
+
+scp cluster2:/home/master0/users/d/darlan/find_codebooks/*.mat ./
+scp cluster2:/home/master0/users/d/darlan/find_codebooks/*.npz ./
---
+++ apps/codebooks/minimum_distance_all_codebooks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""module docstring"""
+
+from subspace.metrics import calcChordalDistance, calcChordalDistanceFromPrincipalAngles, calcPrincipalAngles
+
+import numpy as np
+
+Nt = 2
+Ns = 1
+# Serão gerados um script e um arquivo de configuraçao para cada valor de K
+K = range(8, 65, 4)
+
+saved_data_file = "codebook_{0}_precoders_in_G({1},{2}).npz"
+
+np.set_printoptions(precision=4)
+min_dists = ""
+for k in K:
+    #print saved_data_file.format(k, Nt, Ns)
+    results = np.load(saved_data_file.format(k, Nt, Ns))
+    min_dists += " | {:0.4f}".format(results['best_dist'].item())
+    # Parte na metade
+    if k == 36:
+        min_dists += " |\n"
+
+    #print "{0} & {1}".format(k, results['best_dist'])
+
+min_dists += " |"
+
+print min_dists
