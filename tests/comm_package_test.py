Fixed a bug in the calcTheoreticalSpectralEfficiency (the calculation was wrong) and its test.
git-svn-id: http://py-physim.googlecode.com/svn/trunk@189 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

tests/comm_package_test.py
@@ -1334,9 +1334,9 @@ class PSKTestCase(unittest.TestCase):
         se2 = self.psk_obj.calcTheoreticalSpectralEfficiency(SNRs, L2)
 
         K = self.psk_obj.K
-        expected_se = K * self.psk_obj.calcTheoreticalPER(SNRs, 1)
-        expected_se1 = K * self.psk_obj.calcTheoreticalPER(SNRs, L1)
-        expected_se2 = K * self.psk_obj.calcTheoreticalPER(SNRs, L2)
+        expected_se = K * (1 - self.psk_obj.calcTheoreticalPER(SNRs, 1))
+        expected_se1 = K * (1 - self.psk_obj.calcTheoreticalPER(SNRs, L1))
+        expected_se2 = K * (1 - self.psk_obj.calcTheoreticalPER(SNRs, L2))
 
         np.testing.assert_array_almost_equal(se, expected_se)
         np.testing.assert_array_almost_equal(se1, expected_se1)