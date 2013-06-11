749c9bbf6b8d18bbefcd1e8ee1f513d04b7d0eba
Darlan Cavalcante Moreira <darcamo@gmail.com>
2013-06-11 21:17:17 +0000

    Improved the configobj example of usage.
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@295 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

--- apps/configobj_usage_example.py
+++ apps/configobj_usage_example.py
@@ -9,10 +9,11 @@ import validate
 from validate import Validator, VdtTypeError
 import numpy as np
 
-def _parse_range_expr(value):
-    """Parse a string in the form of min:max or min:step:max and return a numpy
-    array.
 
+def _parse_range_expr(value):
+    """
+    Parse a string in the form of min:max or min:step:max and return a
+    numpy array.
     """
     try:
         limits = value.split(':')
@@ -27,12 +28,30 @@ def _parse_range_expr(value):
     return value
 
 
-def real_numpy_array_check(value, minv=None, maxv=None):
-    """Value can be either a single number, a range expression in the form of
+# TODO: Check agains the 'min' and 'max'. For now they are ignored.
+def real_numpy_array_check(value, min=None, max=None):
+    """Parse and validate `value` as a numpy array.
+
+    Value can be either a single number, a range expression in the form of
     min:max or min:step:max, or even a list containing numbers and range
     expressions.
 
+    Notes:
+    ------
+    You can either separate the values with commas or spaces. However, if
+    you separate with spaces the values should be brackets, while if you
+    separate with commands there should be no brackets.
+    .. code::
+        SNR = 0,5,10:20
+        SNR = [0 5 10:20]
     """
+    if isinstance(value, str):
+        # Remove '[' and ']' if they exist.
+        if value[0] == '[' and value[-1] == ']':
+            value = value[1:-1].strip()
+            value = value.split()
+
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     # Test if it is a list or not
     if isinstance(value, list):
         # If it is a list, each element can be either a number of a 'range
@@ -47,7 +66,7 @@ def real_numpy_array_check(value, minv=None, maxv=None):
         # expression' that can be parsed with _parse_range_expr
         try:
             value = validate.is_float(value)
-            value=np.array([value])
+            value = np.array([value])
         except VdtTypeError:
             value = _parse_range_expr(value)
 
@@ -58,17 +77,29 @@ if __name__ == '__main__':
     config_file_name = 'psk_simulation_config.txt'
 
     # Spec could be also the name of a file containing the string below
-    spec = """[Simulation]
-    SNR=real_numpy_array
+    spec = """[Scenario]
+    SNR=real_numpy_array(default=15)
+    modulator=option('PSK', 'QAM', 'BPSK', default="PSK")
     M=integer(min=4, max=512, default=4)
-    NSymbs=integer(min=10, max=1000000, default=500)
-    rep_max=integer(min=1, default=5000)
-    max_bit_errors=integer(min=1, default=300)""".split("\n")
+    NSymbs=integer(min=10, max=1000000, default=200)
+    K=integer(min=2,default=3)
+    Nr=integer(min=2,default=2)
+    Nt=integer(min=2,default=2)
+    Ns=integer(min=1,default=1)
+    [IA Algorithm]
+    max_iterations=integer(min=1, default=60)
+    [General]
+    rep_max=integer(min=1, default=2000)
+    max_bit_errors=integer(min=1, default=3000)
+    unpacked_parameters=string_list(default=list('SNR'))
+    """.split("\n")
 
     conf_file_parser = ConfigObj(
         config_file_name,
         list_values=True,
         configspec=spec)
+
+    #conf_file_parser.write()
 
     # Dictionary with custom validation functions
     fdict = {'real_numpy_array': real_numpy_array_check}
@@ -107,17 +138,30 @@ if __name__ == '__main__':
     print 'Filename: {0}'.format(config_file_name)
     print "Valid_config_file: {0}".format(result)
 
-    SimulationSection = conf_file_parser['Simulation']
+    # Simulation Section
+    SimulationSection = conf_file_parser['Scenario']
     SNR = SimulationSection['SNR']
     M = SimulationSection['M']
     NSymbs = SimulationSection['NSymbs']
-    rep_max = SimulationSection['rep_max']
-    max_bit_errors = SimulationSection['max_bit_errors']
-    #dummy = SimulationSection['dummy']
+    modulator_name = SimulationSection['modulator']
+
+    # IA Section
+    IASection = conf_file_parser['IA Algorithm']
+    max_iterations = IASection['max_iterations']
+
+    # General Section
+    GeneralSection = conf_file_parser['General']
+    rep_max = GeneralSection['rep_max']
+    max_bit_errors = GeneralSection['max_bit_errors']
+    unpacked_parameters = GeneralSection['unpacked_parameters']
 
     print "Parameters: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     print "SNR: {0}".format(SNR)
     print "M: {0}".format(M)
+    print "Modulator: {0}".format(modulator_name)
     print "NSymbs: {0}".format(NSymbs)
     print "rep_max: {0}".format(rep_max)
     print "max_bit_errors: {0}".format(max_bit_errors)
+    print "unpacked_parameters: {0}".format(unpacked_parameters)
+
+    conf_file_parser.write()
