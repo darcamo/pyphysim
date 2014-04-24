a5b45beea5924074ee8d55da694aaf41a00880a1
Darlan Cavalcante Moreira <darcamo@gmail.com>
2014-04-24 19:55:12 +0000

    Finish the implementation of the split_into_partial_results.py script.
    git-svn-id: http://py-physim.googlecode.com/svn/trunk@576 52f46f9c-28e8-69bd-04f8-aaf5e2abe915

--- bin/split_into_partial_results.py
+++ bin/split_into_partial_results.py
@@ -22,6 +22,7 @@ from pyphysim.simulations.runner import get_partial_results_filename
 from pyphysim.util.misc import replace_dict_values
 
 
+# TODO: Test if the partial results saved by this script are correct
 def main():
     """Main function of the script.
     """
@@ -33,33 +34,55 @@ def main():
     args = parser.parse_args()
 
     name = args.name
-
-    if args.folder is None:
-        folder = 'subsolfer'
-    else:
-        folder = args.folder
-
-    print name
-    print folder
+    folder = args.folder  # This will be None, if not provided
 
     results = SimulationResults.load_from_file(name)
+
     original_filename = results.original_filename
 
-    for i, p in enumerate(results.params.get_unpacked_params_list()):
-        print i
-        partial_filename = get_partial_results_filename(original_filename, p)
+    # xxxxxxxxxx Remove the .pickle file extension xxxxxxxxxxxxxxxxxxxxxxxx
+    # If partial_filename has a 'pickle' extension we remove it from the name
+    original_filename_no_ext, ext = os.path.splitext(original_filename)
+    if ext == '.pickle':
+        original_filename = original_filename_no_ext
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+    unpacked_params_list = results.params.get_unpacked_params_list()
+    all_results_names = results.get_result_names()
+
+    for i, p in enumerate(results.params.get_unpacked_params_list()):
+        partial_filename = get_partial_results_filename(original_filename, p, folder)
         partial_filename_with_replacements = replace_dict_values(
             partial_filename,
             results.params.parameters,
             filename_mode=True)
+
         if partial_filename_with_replacements == name:
             raise RuntimeError('invalid name')
-        print partial_filename_with_replacements
 
+        partial_param = unpacked_params_list[i]
+        partial_result = SimulationResults()
+        partial_result.set_parameters(partial_param)
 
-    # TODO: Finish the implementation
-    # Save the partial results
+        for result_name in all_results_names:
+            partial_result.add_result(results[result_name][i])
+
+        partial_result.current_rep = results.runned_reps[i]
+
+        # Try to save the partial results. If we get an error
+        try:
+            # If we get an IOError exception here, maybe we are trying to
+            # save to a folder and the folder does not exist yet.
+            partial_result.save_to_file(partial_filename_with_replacements)
+        except IOError as e:
+            if folder is not None:
+                # Lets create the folder...
+                os.mkdir(folder)
+                # ... and try to save the file again.
+                partial_result.save_to_file(partial_filename_with_replacements)
+            else:
+                raise e
+
 
 if __name__ == '__main__':
     main()
