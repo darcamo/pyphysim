56c17965d455071f2acd4aa9a68e14d8bec25f65
Darlan Cavalcante Moreira <darcamo@gmail.com>
2014-11-12 17:57:07 -0300

    Add visualization for the SINR in each room

--- apps/simulate_metis_scenario.py
+++ apps/simulate_metis_scenario.py
@@ -17,24 +17,13 @@ except NameError:
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 # xxxxxxxxxx Import Statements xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-import math
-import cmath
-from time import time
 import numpy as np
-from pprint import pprint
+from matplotlib import pyplot as plt
+import matplotlib as mpl
 
-from pyphysim.simulations.runner import SimulationRunner
-from pyphysim.simulations.parameters import SimulationParameters
-from pyphysim.simulations.results import SimulationResults, Result
-from pyphysim.simulations.simulationhelpers import simulate_do_what_i_mean, \
-    get_common_parser
-from pyphysim.comm import modulators, channels
 from pyphysim.util.conversion import dB2Linear, dBm2Linear, linear2dB
-from pyphysim.util import misc
-from pyphysim.ia import algorithms
 from pyphysim.cell import cell
 from pyphysim.comm import pathloss
-from pyphysim.simulations.progressbar import ProgressbarText
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
@@ -83,6 +72,37 @@ def get_cell_users_indexes(cell_index, num_users_per_cell):
     return np.arange(0, num_users_per_cell) + cell_index * num_users_per_cell
 
 
+def prepare_sinr_array_for_color_plot(sinr_array,
+                                      num_cells_per_side,
+                                      num_discrete_positions_per_cell):
+    """
+
+    Parameters
+    ----------
+    sinr_array : TYPE
+    num_cells_per_side : TYPE
+    num_discrete_positions_per_cell : TYPE
+    """
+    # dummy = np.zeros(
+    #     [num_cells,
+    #      num_discrete_positions_per_cell * num_discrete_positions_per_cell])
+    dummy = sinr_array
+    # for cell_idx in range(num_cells):
+    #     dummy[cell_idx] = cell_idx
+
+    dummy2 = dummy.reshape(
+        [num_cells_per_side,
+         num_cells_per_side,
+         num_discrete_positions_per_cell,
+         num_discrete_positions_per_cell])
+
+    dummy3 = np.swapaxes(dummy2, 1, 2).reshape(
+        [num_cells_per_side * num_discrete_positions_per_cell,
+         num_cells_per_side * num_discrete_positions_per_cell],
+        order='C')
+    return dummy3
+
+
 if __name__ == '__main__':
     # xxxxxxxxxx Simulation Configuration xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
     side_length = 10
@@ -102,7 +122,7 @@ if __name__ == '__main__':
 
     # Transmit Power and noise
     Pt = dBm2Linear(20)  # 20 dBm transmit power
-    noise_var = dBm2Linear(-97)
+    noise_var = 0.0  # dBm2Linear(-97)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Create the cluster xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -111,15 +131,16 @@ if __name__ == '__main__':
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Calculate cell positions and wall losses xxxxxxxxxxxxxxxxx
-    cell_positions = np.array([c.pos for c in cluster._cells])
+    cell_positions = np.array([c.pos for c in cluster])
     wall_losses_dB = calc_wall_losses(side_length, cell_positions)
     wall_losses = dB2Linear(-wall_losses_dB)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Create the path loss object xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    # pl_obj = pathloss.PathLoss3GPP1()
-    pl_obj = pathloss.PathLossFreeSpace()
-    pl_obj.handle_small_distances_bool = True
+    pl_3gpp_obj = pathloss.PathLoss3GPP1()
+    pl_free_space_obj = pathloss.PathLossFreeSpace()
+    pl_3gpp_obj.handle_small_distances_bool = True
+    pl_free_space_obj.handle_small_distances_bool = True
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Add one user in each position of each room xxxxxxxxxxxxxxx
@@ -131,7 +152,14 @@ if __name__ == '__main__':
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Output SINR vector xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    sinr_array = np.zeros([num_cells, num_users_per_cell], dtype=float)
+    # One output for each case: no pathloss, 3GPP path loss and free space
+    # path loss
+    sinr_array_pl_nothing = np.zeros(
+        [num_cells, num_users_per_cell], dtype=float)
+    sinr_array_pl_3gpp = np.zeros(
+        [num_cells, num_users_per_cell], dtype=float)
+    sinr_array_pl_free_space = np.zeros(
+        [num_cells, num_users_per_cell], dtype=float)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     # xxxxxxxxxx Let's do the simulations xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -140,20 +168,26 @@ if __name__ == '__main__':
 
     # xxxxxxxxxx Calculate the distance and path losses xxxxxxxxxxxxxxx
     dists = cluster.calc_dist_all_users_to_each_cell()
-    pl = pl_obj.calc_path_loss(dists)
+    pl_3gpp = pl_3gpp_obj.calc_path_loss(dists)
+    pl_free_space = pl_free_space_obj.calc_path_loss(dists)
+    pl_nothing = np.ones([num_cells * num_users_per_cell, num_cells],
+                         dtype=float)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
     for cell_idx in range(num_cells):
         # Index of the users in the current cell
         users_idx = get_cell_users_indexes(cell_idx, num_users_per_cell)
 
-        # Get the desired power of the users in the cell
-        desired_power = Pt * pl[users_idx, cell_idx]
-
         # Mask to get path loss of all transmitters ...
         mask = np.ones(num_cells, dtype=bool)
         # ... except the desired transmitter
         mask[cell_idx] = 0
+
+        # xxxxxxxxxx Case without pathloss xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        pl = pl_nothing
+
+        # Get the desired power of the users in the cell
+        desired_power = Pt * pl[users_idx, cell_idx]
 
         # Calculate the sum of all interference powers
         undesired_power = np.sum(
@@ -163,14 +197,75 @@ if __name__ == '__main__':
         # Calculate the SINR of the user
         sinr_users_in_current_cell = (desired_power /
                                       (undesired_power + noise_var))
-        sinr_array[cell_idx, :] = sinr_users_in_current_cell
+        sinr_array_pl_nothing[cell_idx, :] = sinr_users_in_current_cell
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-    sinr_array_dB = linear2dB(sinr_array)
+        # xxxxxxxxxx Case with 3GPP pathloss xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        pl = pl_3gpp
+
+        # Get the desired power of the users in the cell
+        desired_power = Pt * pl[users_idx, cell_idx]
+
+        # Calculate the sum of all interference powers
+        undesired_power = np.sum(
+            Pt * pl[users_idx][:, mask] * wall_losses[cell_idx, mask],
+            axis=-1)
+
+        # Calculate the SINR of the user
+        sinr_users_in_current_cell = (desired_power /
+                                      (undesired_power + noise_var))
+        sinr_array_pl_3gpp[cell_idx, :] = sinr_users_in_current_cell
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+        # xxxxxxxxxx Case with Free Space path loss xxxxxxxxxxxxxxxxxxxxxxx
+        pl = pl_free_space
+
+        # Get the desired power of the users in the cell
+        desired_power = Pt * pl[users_idx, cell_idx]
+
+        # Calculate the sum of all interference powers
+        undesired_power = np.sum(
+            Pt * pl[users_idx][:, mask] * wall_losses[cell_idx, mask],
+            axis=-1)
+
+        # Calculate the SINR of the user
+        sinr_users_in_current_cell = (desired_power /
+                                      (undesired_power + noise_var))
+        sinr_array_pl_free_space[cell_idx, :] = sinr_users_in_current_cell
+        # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    # xxxxxxxxxx Convert values to dB xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    sinr_array_pl_nothing_dB = linear2dB(sinr_array_pl_nothing)
+    sinr_array_pl_3gpp_dB = linear2dB(sinr_array_pl_3gpp)
+    sinr_array_pl_free_space_dB = linear2dB(sinr_array_pl_free_space)
     # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    # mean_sinrs = linear2dB(sinr_array).mean(axis=0)
-    # print mean_sinrs
 
-    # print np.mean(mean_sinrs)
-    print "Mean SINR value: {0}".format(sinr_array_dB.mean())
+    print "Mean SINR value (no PL): {0}".format(
+        sinr_array_pl_nothing_dB.mean())
+    print "Mean SINR value (3GPP): {0}".format(sinr_array_pl_3gpp_dB.mean())
+    print "Mean SINR value (Free Space): {0}".format(
+        sinr_array_pl_free_space_dB.mean())
 
-    # cluster.plot()
+    # xxxxxxxxxx Prepare data to be plotted xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    sinr_array_pl_nothing_dB2 = prepare_sinr_array_for_color_plot(
+        sinr_array_pl_nothing_dB)
+    sinr_array_pl_3gpp_dB2 = prepare_sinr_array_for_color_plot(
+        sinr_array_pl_3gpp_dB)
+    sinr_array_pl_free_space_dB2 = prepare_sinr_array_for_color_plot(
+        sinr_array_pl_free_space_dB)
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    # xxxxxxxxxx Plot each case xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    # No path loss
+    ax1 = plt.matshow(sinr_array_pl_nothing_dB2)
+    plt.colorbar()
+
+    # 3GPP path loss
+    ax2 = plt.matshow(sinr_array_pl_3gpp_dB2)
+    plt.colorbar()
+
+    ax3 = plt.matshow(sinr_array_pl_free_space_dB2)
+    plt.colorbar()
+
+    plt.show()
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
