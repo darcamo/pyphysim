816360ed24ac1d3db84e2753aefb86d3ef4c7733
Darlan Cavalcante Moreira <darcamo@gmail.com>
2015-11-05 02:29:22 -0300

    Use current interpreter for coverage script
    Change bin/run_python_coverage.sh so that it uses the current
    interpreter for reporting the tests coverage. That means that you should
    install 'coverage' (using pip) in some virtualenv environment and
    activate the environment before running the script.

--- bin/run_python_coverage.sh
+++ bin/run_python_coverage.sh
@@ -1,10 +1,10 @@
 # Example in http://nedbatchelder.com/code/coverage/cmd.html
 
-# You need to install the python-coverage package (available in the Ubuntu
+# You need to install the coverage package (available in the Ubuntu
 # repositories) to run this script.
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-# This script run python-coverage on each test file here and append the
+# This script run coverage on each test file here and append the
 # data to the .coverage file. Then it generates a coverage report.
 # 
 # Note that the coverage report omits stuff on the /usr folder to avoid
@@ -12,7 +12,7 @@
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-# Observation: Ideally it is better to run python-coverage on a single test
+# Observation: Ideally it is better to run coverage on a single test
 # file and generate the report only for that test file. In the report one
 # would care only about the coverage of the tested module.
 #
@@ -28,14 +28,14 @@ cd ../tests
 rm -f .coverage
 for file in *test.py;
 do
-    echo "xxxxxxxxxx Running python-coverage on file $file"
-    python-coverage run -a --omit "/usr/*" $file > /dev/null;
+    echo "xxxxxxxxxx Running coverage on file $file"
+    coverage run -a --omit "/usr/*" --source "../pyphysim" $file > /dev/null;
     echo "xxxxxxxxxx Done xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 done
 
 # Generate the report
 echo "xxxxxxxxxx Creating Coverage Report xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-python-coverage report -m --omit "/usr/*","/*__init__*","/*setup.*","/*apps/*"
+coverage report -m --omit "/usr/*","/*__init__*","/*setup.*","/*apps/*"
 echo "xxxxxxxxxx Done xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 
