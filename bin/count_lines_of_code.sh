d9d28bab01855caf577986e3ef56803b0d683ac4
Darlan Cavalcante Moreira <darcamo@gmail.com>
2015-11-10 23:34:07 -0300

    Update count_lines_of_code.sh file

--- bin/count_lines_of_code.sh
+++ bin/count_lines_of_code.sh
@@ -1,6 +1,13 @@
 # This requires the cloc program to be installed.
 cd ..
-cloc apps cell comm comp ia MATLAB plot subspace util --exclude-lang="Bourne Shell,C"
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo Lines of codes in Pyphysim library
+cloc pyphysim --exclude-lang="Bourne Shell,C"
+
+echo
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo Lines of codes in simulators
+cloc apps --exclude-lang="Bourne Shell"
 
 echo
 echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
